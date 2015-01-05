1d6ef18aecd145c0d989ea0e72e109f21b372ca3
vscom <surfacedust@163.com>
2015-01-05 17:41:31 +0800

    add the func bfs,new-path

--- test_pro.lisp
+++ test_pro.lisp
@@ -44,3 +44,20 @@
 
 (defun shortest-path(start end net)
   (bfs end (list (list start)) net))
+
+(defun bfs (end queue net)
+  (if (null queue)
+      nil
+      (let ((path (car queue)))
+	(let ((node (car path)))
+	  (if (eql node end)
+	      (reverse path)
+	      (bfs end
+		   (append (cdr queue)
+			   (new-paths path node net))
+		   net))))))
+
+(defun new-path (path node net)
+  (mapcar #'(lambda (n)
+	      (cons n path))
+	  (cdr (assoc node net))))
