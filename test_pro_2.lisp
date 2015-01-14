5b51c7adb9c640fbfcdb62208ff184631735b564
vscom <surfacedust@163.com>
2015-01-14 17:30:41 +0800

    add func parse-date

--- test_pro_2.lisp
+++ test_pro_2.lisp
@@ -48,3 +48,30 @@
 (defun constituent (c)
   (and (graphic-char-p c)
        (not (char= c #\ ))))
+
+
+(defun parse-date (str)
+  (let ((toks (tokens str #'constituent 0)))
+    (list (parse-integer (first toks))
+	  (parse-month (second toks))
+	  (parse-integer (third toks)))))
+
+(defconstant month-names
+  #("jan" "feb" "mar" "apr" "may" "jun"
+    "jul" "aug" "sep" "oct" "nov" "dec"))
+
+(defun parse-month (str)
+  (let ((p (position str month-names
+                         :test #'string-equal)))
+    (if p
+        (+ p 1)
+        nil)))
+
+(defun read-integer (str)
+  (if (every #'digit-char-p str)
+      (let ((accum 0))
+	(dotimes (pos (length str))
+	  (setf accum (+ (* accum 10)
+			 (digit-char-p (char str pos)))))
+	accum)
+      nil))
